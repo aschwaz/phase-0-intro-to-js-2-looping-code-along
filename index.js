82cd843203536f29f7454fda643c27fd07912e62
Andrew Schwartz <andrewschwartz525@gmail.com>
2023-08-01 17:09:49 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+
+const names = ["Andrew", "Mark", "Connor"]
+const day = "birthday"
+
+function writeCards(names, day) {
+    let messages = []
+    for(let i=0; i < names.length; i++) {   
+    messages =[...messages, `Thank you, ${names[i]}, for the wonderful ${day} gift!`]
+    }
+    return (messages);
+}
+
+console.log(writeCards(names, day));
+
+function countDown (number) {
+    while (number >=0) {
+    console.log(number--);
+    }
+}
+countDown(10);
